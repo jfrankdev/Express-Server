479da7ae6c4c732d2acdc71e126df5674f108a50
jfrankdev <jfrankdev90@gmail.com>
2016-10-23 11:27:10 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+//Allow all requests from all domains & localhost
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+var ingredients = [{"id":1,"text":"ham"}, {"id":2,"text":"ham"},{"id":3,"text":"potatoes"}];
+
+
+app.get('/ingredients', function(req, res) {
+    console.log("GET From Here");
+    res.send(ingredients);
+});
+
+app.post('/ingredients', function(req, res) {
+    var ingredient = req.body;
+    console.log(req.body);
+    ingredients.push(ingredient);
+    res.status(200).send("Successfully posted ingredient");
+});
+
+app.listen(6060);
