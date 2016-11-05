037cfeeb10621174eed303c97b8bef2ca037162e
jfrankdev <jfrankdev90@gmail.com>
2016-11-05 00:33:24 -0800

    Added mock data

--- server.js
+++ server.js
@@ -15,83 +15,63 @@ app.all('/*', function(req, res, next) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var mcdYes = [{"id":"McDonalds","text":"5"}];
-var mcdNo = [{"id":3,"text":"8"}];
-var bkYes = [{"id":"Burger King","text":"2"}];
-var bkNo = [{"id":3,"text":"6"}];
-var domYes = [{"id":"Dominos","text":"7"}];
-var domNo = [{"id":3,"text":"3"}];
-var phutYes = [{"id":"Pizza Hut","text":"3"}];
-var phutNo = [{"id":3,"text":"7"}];
-var arbYes = [{"id":"Arbys","text":"3"}];
-var arbNo = [{"id":3,"text":"7"}];
-var wendYes = [{"id":"wendys","text":"3"}];
-var wendNo = [{"id":3,"text":"7"}];
 
-app.get('/wendYes', function(req, res) {
-    res.send(wendYes);
-});
 
-app.get('/wendNo', function(req, res) {
-    res.send(wendNo);
-});
 
-app.get('/arbYes', function(req, res) {
-    res.send(arbYes);
-});
+var bkYes = [{"id": "Burger King","vote": -1}];
+var mcdYes = [{"id": "Mcdonalds","vote": -1}];
+var domYes = [{"id": "Dominos","vote": -1}];
+var phutYes = [{"id": "Pizza Hut","vote": -1}];
+var arbyYes = [{"id": "Arbys","vote": -1}];
+var WendYes = [{"id": "Wendys","vote": -1}];
 
-app.get('/arbNo', function(req, res) {
-    res.send(arbNo);
-});
 
-app.get('/phutYes', function(req, res) {
-    res.send(phutYes);
-});
 
-app.get('/phutNo', function(req, res) {
-    res.send(phutNo);
-});
-
-app.get('/domYes', function(req, res) {
-    res.send(domYes);
-});
-
-app.get('/domNo', function(req, res) {
-    res.send(domNo);
-});
-
-app.get('/mcdYes', function(req, res) {
-    res.send(mcdYes);
-});
-
-app.get('/mcdNo', function(req, res) {
-    res.send(mcdNo);
-});
 
 app.get('/bkYes', function(req, res) {
     res.send(bkYes);
 });
-
-app.get('/bkNo', function(req, res) {
-    res.send(bkNo);
+app.get('/mcdYes', function(req, res) {
+    res.send(mcdYes);
+});
+app.get('/domYes', function(req, res) {
+    res.send(domYes);
+});
+app.get('/phutYes', function(req, res) {
+    res.send(phutYes);
+});
+app.get('/arbyYes', function(req, res) {
+    res.send(arbyYes);
+});
+app.get('/WendYes', function(req, res) {
+    res.send(WendYes);
 });
 
-var ingredients = [
-    {
-        "id": "Burger King",
-        "vote": -1
-    }
-];
 
-app.get('/ingredients', function(req, res) {
-    console.log("GET From SERVER");
-    res.send(ingredients);
+
+
+app.post('/bkYes', function(req, res) {
+    bkYes[0].vote++;
+    res.status(200).send("Successfully posted ingredient");
 });
-
-app.post('/ingredients', function(req, res) {
-
-    ingredients[0].vote++;
-
+app.post('/mcdYes', function(req, res) {
+    mcdYes[0].vote++;
+    res.status(200).send("Successfully posted ingredient");
+});
+app.post('/domYes', function(req, res) {
+    domYes[0].vote++;
+    res.status(200).send("Successfully posted ingredient");
+});
+app.post('/phutYes', function(req, res) {
+    phutYes[0].vote++;
+    res.status(200).send("Successfully posted ingredient");
+});
+app.post('/arbyYes', function(req, res) {
+    arbyYes[0].vote++;
+    res.status(200).send("Successfully posted ingredient");
+});
+app.post('/WendYes', function(req, res) {
+    WendYes[0].vote++;
     res.status(200).send("Successfully posted ingredient");
 });
 
